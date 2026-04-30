e8a47417485231a6f95d58f37df1bfe6dc26bdfd
Peyman Aparviz <peyman.aparviz@gmail.com>
2026-05-01 00:17:23 +0200

    init project

---
+++ mytour/src/test/java/org/fhtw/mytour/MytourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fhtw.mytour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
